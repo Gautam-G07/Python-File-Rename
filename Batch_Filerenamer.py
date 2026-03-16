54cb704bf41ce8ee3055d071d8bf72777e0dd350
Gautam G <gautam26g@gmail.com>
2026-03-16 20:08:39 +0530

    Built batch file renamer using os module

--- Batch_Filerenamer.py
+++ Batch_Filerenamer.py
@@ -3,9 +3,11 @@ import shutil
 folder_path=r"D:\C programming codes 3rd sem lab"
 files=os.listdir(folder_path)
 for i, file in enumerate(files, start=1):
-    old=os.path.join(folder_path, file)
-    name,extension=os.path.splitext(file)
-    new_name=f"{i}{extension}"
-    new=os.path.join(folder_path, new_name)
-    os.rename(old, new) 
+    if file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg") :
+        old=os.path.join(folder_path, file)
+        name,extension=os.path.splitext(file)
+        new_name=f"{i}{extension}"
+        new=os.path.join(folder_path, new_name)
+        os.rename(old, new) 
 print("All files have been renamed successfully.")  
+
