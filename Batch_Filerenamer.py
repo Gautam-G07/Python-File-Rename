4e083d6e06592e0795791c3de2f52852ccc742c8
Gautam G <gautam26g@gmail.com>
2026-03-16 20:06:21 +0530

    Final update

--- Batch_Filerenamer.py
+++ Batch_Filerenamer.py
@@ -1,8 +1,11 @@
 import os 
 import shutil
-folder_path=r"C:\Users\HP\Desktop\New folder (2)"
+folder_path=r"D:\C programming codes 3rd sem lab"
 files=os.listdir(folder_path)
 for i, file in enumerate(files, start=1):
     old=os.path.join(folder_path, file)
     name,extension=os.path.splitext(file)
     new_name=f"{i}{extension}"
+    new=os.path.join(folder_path, new_name)
+    os.rename(old, new) 
+print("All files have been renamed successfully.")  
