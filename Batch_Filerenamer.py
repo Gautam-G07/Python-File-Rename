8b49a4b5b717dc350e279f21cbe81e72f4c7327a
Gautam G <gautam26g@gmail.com>
2026-03-17 19:44:25 +0530

    Day 9 - Improved batch renamer with filters and custom prefix

--- Batch_Filerenamer.py
+++ Batch_Filerenamer.py
@@ -1,12 +1,13 @@
 import os 
 import shutil
 folder_path=r"D:\C programming codes 3rd sem lab"
+prefix=input("Enter a prefix for the renamed files: ")
 files=os.listdir(folder_path)
 for i, file in enumerate(files, start=1):
-    if file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg") :
+    if file.endswith((".jpg", ".jpeg", ".png")) and os.path.isfile(os.path.join(folder_path, file)):
         old=os.path.join(folder_path, file)
         name,extension=os.path.splitext(file)
-        new_name=f"{i}{extension}"
+        new_name=f"{prefix}_{i}{extension}"
         new=os.path.join(folder_path, new_name)
         os.rename(old, new) 
 print("All files have been renamed successfully.")  
